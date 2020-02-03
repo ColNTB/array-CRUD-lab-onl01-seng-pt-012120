6c2ad4f37feb4ced76d50268a2528f37bbda5af4
Nathan Bohn <nathantbohn@gmail.com>
2020-02-03 13:13:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,21 +1,21 @@
 def create_an_empty_array
-  
+    myArray = []
 end
 
 def create_an_array
-  
+    myArray = [1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+    array.unshift(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+    element = array.pop
 end
 
 def remove_element_from_end_of_array(array)
-  
+    element
 end
 
 def remove_element_from_start_of_array(array)
